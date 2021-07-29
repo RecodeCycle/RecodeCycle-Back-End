27406a72a15898ea1c9688d09c860536adedba54
Gabriel Almeida <86261232+bielalmd@users.noreply.github.com>
2021-07-29 10:45:29 -0300

    Add files via upload

---
+++ recodecycle/src/test/java/br/com/recodecycle/RecodecycleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.recodecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecodecycleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
