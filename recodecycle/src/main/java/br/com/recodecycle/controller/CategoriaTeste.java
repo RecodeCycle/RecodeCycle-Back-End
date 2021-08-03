845cd4727e950737b2d78f293f593fe7449776b9
Gabriel Almeida <86261232+bielalmd@users.noreply.github.com>
2021-08-03 10:16:28 -0300

    Projeto Integrador

---
+++ recodecycle/src/main/java/br/com/recodecycle/controller/CategoriaTeste.java
@@ -0,0 +1,5 @@
+package br.com.recodecycle.controller;
+
+public class CategoriaTeste {
+
+}
--- recodecycle/src/main/java/br/com/recodecycle/repository/CategoriaRepository.java
+++ recodecycle/src/main/java/br/com/recodecycle/repository/CategoriaRepository.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package br.com.recodecycle.repository;
 
 import java.util.List;
@@ -12,18 +11,3 @@ public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
 	public List <Categoria> findAllByDescricaoContainingIgnoreCase(String descricao);
 	
 }
-=======
-package br.com.recodecycle.repository;
-
-import java.util.List;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.recodecycle.model.Categoria;
-
-public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
-
-	public List <Categoria> findAllByDescricaoContainingIgnoreCase(String descricao);
-	
-}
->>>>>>> a380b24f5b48925eecb285590c94bb3367ee2395
