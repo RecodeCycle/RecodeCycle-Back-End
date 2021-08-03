edf3e5c87c8baa40b8264f2a06acb4a625cec08a
Garrido10 <fernando.garrido98@gmail.com>
2021-08-03 11:26:43 -0300

    Categoria Repository

---
+++ recodecycle/src/main/java/br/com/recodecycle/repository/CategoriaRepository.java
@@ -0,0 +1,12 @@
+package br.com.recodecycle.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.recodecycle.model.Categoria;
+
+public interface CategoriaRepository extends JpaRepository <Categoria, Long> {
+    public List<Categoria> findAllByDescricaoContainingIgnoreCase (String descricao);    
+
+}
