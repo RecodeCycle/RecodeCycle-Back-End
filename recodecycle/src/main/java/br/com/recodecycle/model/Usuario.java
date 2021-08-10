3a5e4d41ee2ae4708b08cb94dfd0eb458b5cab61
93-silvestre <nathyribsilvestre@gmail.com>
2021-08-10 13:54:39 -0300

    Ataualização Security

--- recodecycle/src/main/java/br/com/recodecycle/model/Usuario.java
+++ recodecycle/src/main/java/br/com/recodecycle/model/Usuario.java
@@ -9,6 +9,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -27,6 +28,7 @@ public class Usuario {
 	private String nome;
 	
 	@NotNull
+	@Email
 	@Size(min = 5, max =50, message = "Email Obrigatório")
 	private String email;
 	
