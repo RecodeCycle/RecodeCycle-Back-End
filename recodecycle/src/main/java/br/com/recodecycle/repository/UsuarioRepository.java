a9812f92aea9791b20fb7fe5a5ae204dac68c19a
Gabriel Almeida <86261232+bielalmd@users.noreply.github.com>
2021-08-10 09:20:03 -0300

    usuario repository

---
+++ recodecycle/src/main/java/br/com/recodecycle/repository/UsuarioRepository.java
@@ -0,0 +1,11 @@
+package br.com.recodecycle.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.recodecycle.model.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+	public Optional<Usuario> findByUsuario(String usuario);
+}
