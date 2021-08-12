ada0ab85a8007de8f1a9ead7bdb8e8bc1da92bac
93-silvestre <nathyribsilvestre@gmail.com>
2021-08-12 13:05:50 -0300

    Atualização Deploy

--- recodecycle/src/main/java/br/com/recodecycle/security/BasicSecurityConfig.java
+++ recodecycle/src/main/java/br/com/recodecycle/security/BasicSecurityConfig.java
@@ -15,11 +15,16 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
-	private UserDetailsService userDetailService;
+	private UserDetailsService userDetailsService;
 
 	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userDetailService);
+	protected void configure(AuthenticationManagerBuilder auth)
+	throws Exception {
+	auth.userDetailsService(userDetailsService);
+	auth.inMemoryAuthentication()
+	.withUser("root")
+	.password(passwordEnconder().encode("root"))
+	.authorities("ROLE_USER");
 	}
 	
 	@Bean
